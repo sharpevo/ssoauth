99500a418cb8d5d3f90ce221cecd362273dc7fd2
sharpevo <sharpevo@outlook.com>
2016-10-26 09:25:35 +0800

    Add environment files

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#sed 's,127.0.0.1:9001,'"${DB_PORT#*//}"',g' -i conf/app.conf
+sed 's,runmode.*,'runmode="${RUN_MODE:-dev}"',g' -i conf/app.conf
+sed 's,httpport.*,'httpport="${HTTP_PORT:-8010}"',g' -i conf/app.conf
+sed 's,mongodb.*,'mongodb="${MONGO_DB:-uic}"',g' -i conf/app.conf
+sed 's,mongohost.*,'mongohost="${MONGO_HOST:-db:27017}"',g' -i conf/app.conf
+sed 's,mongouser.*,'mongouser="${MONGO_USER:-uic}"',g' -i conf/app.conf
+sed 's,mongopassword.*,'mongopassword="${MONGO_PASSWORD:-Goo7ieho}"',g' -i conf/app.conf
+sed 's,uicdomain.*,'uicdomain="${UIC_DOMAIN:-accounts.igenetech.com}"',g' -i conf/app.conf
+ssoauth
