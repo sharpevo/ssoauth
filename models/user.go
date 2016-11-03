2e38e0abc11d0c403d8e8411190b5ed21b2787e7
sharpevo <sharpevo@outlook.com>
2016-11-03 13:27:50 +0800

    Change HasDomain to use app collection

--- models/user.go
+++ models/user.go
@@ -41,11 +41,35 @@ func (user *User) HasToken(iss string, token string) bool {
 	return user.Tokens[iss] == token
 }
 
-func (user *User) HasDomain(domain string) bool {
+func (user *User) HasDomain2(domain string) bool {
 	if user.Email == "quwubin@gmail.com" ||
 		domain == beego.AppConfig.DefaultString("uicdomain", "accounts.igenetech.com") {
 		return true
 	} else {
 		return user.Apps[strings.Replace(domain, ".", " ", -1)]
+	}
+}
+
+func (user *User) HasDomain(domain string) bool {
+	if user.Email == "quwubin@gmail.com" ||
+		domain == beego.AppConfig.DefaultString(
+			"uicdomain",
+			"accounts.igenetech.com") {
+		beego.Debug("CheckDomain: true, admin or ask for uic.")
+		return true
+	} else {
+		app := uic.App{}
+
+		session, err := mongo.CopyMasterSession()
+		if err != nil {
+			return false
+		}
+		collection := session.DB(mongo.MongoConfig.Database).C("app")
+		err = collection.Find(bson.M{"domain": domain}).One(&app)
+		beego.Debug("CheckDomain:", user.Apps[app.Id.Hex()], domain, "in", user.Apps)
+		if err != nil {
+			return false
+		}
+		return user.Apps[app.Id.Hex()]
 	}
 }
